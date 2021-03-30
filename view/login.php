c12aa0b0d91decb51840db153890f46f4e789005
mathilderenversade <mathirenversade@gmail.com>
2021-03-30 10:32:59 +0200

    add some styling

--- view/login.php
+++ view/login.php
@@ -12,13 +12,15 @@
     <title>Connexion</title>
   </head>
   <body>
-    <form action="" method="POST">
-        <h1>Connexion</h1>
-        <label>Nom d'utilisateur</label>
-        <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-        <label>Mot de passe</label>
-        <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-        <input type="submit" value="LOGIN">
-    </form>
+    <div id="container">
+        <form action="" method="POST">
+            <h1>Connexion</h1>
+            <label><strong>Nom d'utilisateur</strong></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+            <label><strong>Mot de passe</strong></label>
+            <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+            <input type="submit" value="LOGIN">
+        </form>
+    </div>
   </body>
 </html>
